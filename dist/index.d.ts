c45d8d30f37e8cd34a454c97adb6e3ea362a67c3
hansol <rjxjr55@gmail.com>
2019-05-17 11:06:37 +0900

    0.0.20

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 export { Tree } from "./tree";
 export { INode, IRule, ICommand, POS } from "./rule.interface";
-export declare const LANGO_RULE_VERSION = "v0.0.18";
+export declare const LANGO_RULE_VERSION = "v0.0.20";
