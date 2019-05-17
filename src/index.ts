d5a0054ee57375cfabcdad8de705d792fd3e5d24
hansol <rjxjr55@gmail.com>
2019-05-17 09:50:05 +0900

    0.0.18

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Tree } from "./tree";
 export { INode, IRule, ICommand, POS } from "./rule.interface";
 
-export const LANGO_RULE_VERSION = 'v0.0.17'
+export const LANGO_RULE_VERSION = 'v0.0.18'
